214a06f7f3e0f22febd4670e385888cbd177fafe
Inas Ouda <inasouda@Inass-Laptop.local>
2024-08-09 18:29:19 -0600

    install chakra ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import './App.css'
+import { Button, ButtonGroup } from '@chakra-ui/react'
+
 
 function App() {
 
   return (
     <>
-
+      <Button colorScheme='blue'>Button</Button>
     </>
   )
 }
