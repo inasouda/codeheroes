acdf65b3ba002786aed193fd5c4ba53fa4c275f3
inasouda <74316803+inasouda@users.noreply.github.com>
2024-08-13 23:41:39 -0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,18 @@ import { ChakraProvider } from '@chakra-ui/react';
 import Home from './Home';
 import RegistrationForm from './pages/registration';
 import NavBar from './components/NavBar';
+import FreeLessonForm from './pages/FreeLesson';
 
 const App = () => {
   return (
     <ChakraProvider>
-      <NavBar></NavBar>
       <Router>
+        <NavBar></NavBar>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/register" element={<RegistrationForm />} />
+          <Route path="/free-lesson" element={<FreeLessonForm />} />
+
           {/* Add more routes here for other components/pages */}
         </Routes>
       </Router>
