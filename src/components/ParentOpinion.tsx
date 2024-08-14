a76b16258b510100d86d71798d6084c73e3a5c56
Inas Ouda <inasouda@Inass-Laptop.local>
2024-08-14 10:49:46 -0600

    remove stack from parentopinion

--- src/components/ParentOpinion.tsx
+++ src/components/ParentOpinion.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, Avatar, Text, Stack, Flex } from '@chakra-ui/react';
+import { Box, Avatar, Text, Flex } from '@chakra-ui/react';
 
 interface ParentOpinionProps {
   imageSrc: string;
