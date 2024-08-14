e539577cf81e7efcad82f2294fd170f348d2b887
Inas Ouda <inasouda@Inass-Laptop.local>
2024-08-14 10:00:06 -0600

    update parentopion component

--- src/components/ParentOpinion.tsx
+++ src/components/ParentOpinion.tsx
@@ -10,17 +10,17 @@ interface ParentOpinionProps {
 const ParentOpinion: React.FC<ParentOpinionProps> = ({ imageSrc, parentName, opinion }) => {
   return (
     <Box p={4} borderWidth={1} mb={4} borderRadius="lg" boxShadow="lg" maxW="md" mx="auto" bg="white">
-      <Flex alignItems="flex-start">
-        <Avatar src={imageSrc} size="xl" mr={4} mt={1} />
-        <Stack spacing={2}>
-          <Text fontSize="xl" fontWeight="bold" color="cyan.700">
-            {parentName}
-          </Text>
-          <Text fontSize="md" color="gray.600">
-            {opinion}
-          </Text>
-        </Stack>
+      <Flex alignItems="center">
+        <Avatar src={imageSrc} size="xl" mr={4} />
+        <Text fontSize="xl" fontWeight="bold" color="cyan.700">
+          {parentName}
+        </Text>
       </Flex>
+      <Box mt={4}>
+        <Text fontSize="md" color="gray.600">
+          {opinion}
+        </Text>
+      </Box>
     </Box>
   );
 };
