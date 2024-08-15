c9e4b597b03872eb46eb6eb0b780170b0ec9fb43
Inas Ouda <inasouda@Inass-Laptop.local>
2024-08-14 23:22:39 -0600

    update registration form design

--- src/pages/registration.tsx
+++ src/pages/registration.tsx
@@ -117,148 +117,163 @@ const RegistrationForm: React.FC = () => {
     <Box
       maxW={'100vw'}
       mx="auto"
-      m={8}
       p={6}
-      bg="cyan.50"
+      bg="cyan.500"
       borderWidth={1}
       borderRadius="lg"
       boxShadow="lg"
     >
-      <Heading mb={8}  textAlign="center" color="cyan.600" fontSize="2rem">
+      <Heading mb={8} textAlign="center" color="white" fontSize="2xl" >
         Register
       </Heading>
       <form onSubmit={handleSubmit}>
-        <SimpleGrid columns={{ base: 1, lg: 2 }} spacing={8}>
+        <SimpleGrid columns={{ base: 1, lg: 2 }} spacing={4}>
           {/* Left Column: Personal Information */}
-          <Box>
-            <Box bg="white"   
-                borderWidth={1}
-                borderRadius="lg"
-                boxShadow="lg" 
-                p={4}  mb={{ base: 4, lg: 0 }}>
-                        <Heading size="md" mb={4}>Personal Information</Heading>
-                        <Stack spacing={4}>
-                            <FormControl id="name" isRequired>
-                            <FormLabel fontWeight="bold">Name</FormLabel>
-                            <Input
-                                type="text"
-                                id="name"
-                                value={formData.name}
-                                onChange={handleChange}
-                                placeholder="Enter your name"
-                                fontSize="1rem"
-                            />
-                            </FormControl>
+        
+            <Box
+              bg="white"
+              borderWidth={1}
+              borderRadius="md"
+              boxShadow="md"
+              p={4}
+              // mb={{ base: 4, lg: 0 }}
+            >
+              <Heading size="md" mb={4}>Personal Information</Heading>
+              <Stack spacing={4}>
+                <FormControl id="name" isRequired>
+                  <FormLabel fontSize="lg" fontWeight="medium" color="gray.600">Student Full Name</FormLabel>
+                  <Input
+                    type="text"
+                    id="name"
+                    value={formData.name}
+                    onChange={handleChange}
+                    placeholder="Enter your name"
+                    borderRadius="md"
+                    borderColor="gray.300"
+                    _focus={{ borderColor: "cyan.400", boxShadow: "none" }}
+                    fontSize="1rem"
+                  />
+                </FormControl>
 
-                            <FormControl id="email" isRequired>
-                            <FormLabel fontWeight="bold">Email</FormLabel>
-                            <Input
-                                type="email"
-                                id="email"
-                                value={formData.email}
-                                onChange={handleChange}
-                                placeholder="Enter your email"
-                                fontSize="1rem"
-                            />
-                            </FormControl>
+                <FormControl id="email" isRequired>
+                <FormLabel fontSize="lg" fontWeight="medium" color="gray.600">Email</FormLabel>
+                  <Input
+                    type="email"
+                    id="email"
+                    value={formData.email}
+                    onChange={handleChange}
+                    placeholder="Enter your email"
+                    borderRadius="md"
+                    borderColor="gray.300"
+                    _focus={{ borderColor: "cyan.400", boxShadow: "none" }}
+                    fontSize="1rem"
+                  />
+                </FormControl>
 
-                            <FormControl id="phone" isRequired>
-                            <FormLabel fontWeight="bold">Phone Number</FormLabel>
-                            <Input
-                                type="tel"
-                                id="phone"
-                                value={formData.phone}
-                                onChange={handleChange}
-                                placeholder="Enter your phone number"
-                                fontSize="1rem"
-                            />
-                            </FormControl>
+                <FormControl id="phone" isRequired>
+                  <FormLabel fontSize="lg" fontWeight="medium" color="gray.600">Phone Number</FormLabel>
+                  <Input
+                    type="tel"
+                    id="phone"
+                    value={formData.phone}
+                    onChange={handleChange}
+                    placeholder="Enter your phone number"
+                    borderRadius="md"
+                    borderColor="gray.300"
+                    _focus={{ borderColor: "cyan.400", boxShadow: "none" }}
+                    fontSize="1rem"
+                  />
+                </FormControl>
 
-                            <FormControl id="address" isRequired>
-                            <FormLabel fontWeight="bold">Address</FormLabel>
-                            <Input
-                                type="text"
-                                id="address"
-                                value={formData.address}
-                                onChange={handleChange}
-                                placeholder="Enter your address"
-                                fontSize="1rem"
-                            />
-                            </FormControl>
+                <FormControl id="address" isRequired>
+                <FormLabel fontSize="lg" fontWeight="medium" color="gray.600">Address</FormLabel>
+                  <Input
+                    type="text"
+                    id="address"
+                    value={formData.address}
+                    onChange={handleChange}
+                    placeholder="Enter your address"
+                    borderRadius="md"
+                    borderColor="gray.300"
+                    _focus={{ borderColor: "cyan.400", boxShadow: "none" }}
+                    fontSize="1rem"
+                  />
+                </FormControl>
 
-                            <FormControl id="gender" isRequired>
-                            <FormLabel fontWeight="bold">Gender</FormLabel>
-                            <Select
-                                id="gender"
-                                placeholder="Select your gender"
-                                value={formData.gender}
-                                onChange={handleChange}
-                                fontSize="1rem"
-                            >
-                                <option value="Male">Male</option>
-                                <option value="Female">Female</option>
-                            </Select>
-                            </FormControl>
+                <FormControl id="gender" isRequired>
+                  <FormLabel fontSize="lg" fontWeight="medium" color="gray.600">Gender</FormLabel>
+                  <Select
+                    id="gender"
+                    placeholder="Select your gender"
+                    value={formData.gender}
+                    onChange={handleChange}
+                    borderRadius="md"
+                    borderColor="gray.300"
+                    _focus={{ borderColor: "cyan.400", boxShadow: "none" }}
+                    fontSize="1rem"
+                  >
+                    <option value="Male">Male</option>
+                    <option value="Female">Female</option>
+                  </Select>
+                </FormControl>
 
-                            <FormControl id="dob" isRequired>
-                            <FormLabel fontWeight="bold">Date of Birth</FormLabel>
-                            <Input
-                                type="date"
-                                id="dob"
-                                value={formData.dob}
-                                onChange={handleChange}
-                                fontSize="1rem"
-                            />
-                            </FormControl>
-                        </Stack>
+                <FormControl id="dob" isRequired>
+                <FormLabel fontSize="lg" fontWeight="medium" color="gray.600">Date of Birth</FormLabel>
+                  <Input
+                    type="date"
+                    id="dob"
+                    value={formData.dob}
+                    onChange={handleChange}
+                    borderRadius="md"
+                    borderColor="gray.300"
+                    _focus={{ borderColor: "cyan.400", boxShadow: "none" }}
+                    fontSize="1rem"
+                  />
+                </FormControl>
+              </Stack>
             </Box>
-          </Box>
+      
 
           {/* Right Column: Other Sections */}
-          <Box>
+     
             <Stack spacing={8}>
               {/* Academic and Experience Section */}
-              <Box bg="white" p={4} 
-               borderWidth={1}
-               borderRadius="lg"
-               boxShadow="lg">
+              <Box
+                bg="white"
+                p={4}
+                borderWidth={1}
+                borderRadius="md"
+                boxShadow="md"
+              >
                 <Heading size="md" mb={4}>Academic and Experience</Heading>
                 <Stack spacing={4}>
                   <FormControl id="grade" isRequired>
-                    <FormLabel fontWeight="bold">Grade</FormLabel>
+                  <FormLabel fontSize="lg" fontWeight="medium" color="gray.600">Grade</FormLabel>
                     <Select
                       id="grade"
                       placeholder="Select your grade"
                       value={formData.grade}
                       onChange={handleChange}
+                      borderRadius="md"
+                      borderColor="gray.300"
+                      _focus={{ borderColor: "cyan.400", boxShadow: "none" }}
                       fontSize="1rem"
                     >
-                      {/* {Array.from({ length: 10 }, (_, i) => i + 3).map((grade) => (
-                        <option key={grade} value={grade}>
-                          {grade}
-                        </option>
-                      ))} */}
-                            <option value="" label="Select grade" />
-                        {/* <option value="PRE-K" label="PRE-K" />
-                        <option value="Kindergarten" label="Kindergarten" />
-                        <option value="Grade 1" label="Grade 1" />
-                        <option value="Grade 2" label="Grade 2" />
-                        <option value="Grade 3" label="Grade 3" /> */}
-                        <option value="Grade 4" label="Grade 4" />
-                        <option value="Grade 5" label="Grade 5" />
-                        <option value="Grade 6" label="Grade 6" />
-                        <option value="Grade 7" label="Grade 7" />
-                        <option value="Grade 8" label="Grade 8" />
-                        <option value="Grade 9" label="Grade 9" />
-                        <option value="Grade 10" label="Grade 10" />
-                        <option value="Grade 11" label="Grade 11" />
-                        <option value="Grade 12" label="Grade 12" />
-               
+                      <option value="" label="Select grade" />
+                      <option value="Grade 4" label="Grade 4" />
+                      <option value="Grade 5" label="Grade 5" />
+                      <option value="Grade 6" label="Grade 6" />
+                      <option value="Grade 7" label="Grade 7" />
+                      <option value="Grade 8" label="Grade 8" />
+                      <option value="Grade 9" label="Grade 9" />
+                      <option value="Grade 10" label="Grade 10" />
+                      <option value="Grade 11" label="Grade 11" />
+                      <option value="Grade 12" label="Grade 12" />
                     </Select>
                   </FormControl>
 
                   <FormControl as="fieldset" isRequired>
-                    <FormLabel as="legend" fontWeight="bold">Do you have any experience in coding?</FormLabel>
+                  <FormLabel fontSize="lg" fontWeight="medium" color="gray.600">Do you have any experience in coding?</FormLabel>
                     <RadioGroup
                       onChange={handleCodingExperienceChange}
                       value={formData.codingExperience}
@@ -270,75 +285,88 @@ const RegistrationForm: React.FC = () => {
                     </RadioGroup>
                   </FormControl>
                 </Stack>
-            </Box>
+              </Box>
 
               {/* Consent and Scheduling Section */}
-            <Box bg="white" p={4}
-             borderWidth={1}
-             borderRadius="lg"
-             boxShadow="lg">
+              <Box
+                bg="white"
+                p={4}
+                borderWidth={1}
+                borderRadius="md"
+                boxShadow="md"
+              >
                 <Heading size="md" mb={4}>Consent and Scheduling</Heading>
                 <Stack spacing={4}>
-                <FormControl as="fieldset" id="photoConsent" isRequired>
-                    <FormLabel as="legend" fontWeight="bold">Do you agree to share photos on the website?</FormLabel>
+                  <FormControl as="fieldset" id="photoConsent" isRequired>
+                  <FormLabel fontSize="lg" fontWeight="medium" color="gray.600">Do you agree to share photos on the website?</FormLabel>
                     <RadioGroup
-                    onChange={handlePhotoConsentChange}
-                    value={formData.photoConsent}
+                      onChange={handlePhotoConsentChange}
+                      value={formData.photoConsent}
                     >
-                    <Stack direction="row">
+                      <Stack direction="row">
                         <Radio value="yes" fontSize="1rem">Yes</Radio>
                         <Radio value="no" fontSize="1rem">No</Radio>
-                    </Stack>
+                      </Stack>
                     </RadioGroup>
-                </FormControl>
+                  </FormControl>
 
-                <FormControl id="lessonDay" isRequired>
-                    <FormLabel fontWeight="bold">Choose your lesson day</FormLabel>
+                  <FormControl id="lessonDay" isRequired>
+                  <FormLabel fontSize="lg" fontWeight="medium" color="gray.600">Choose your lesson day</FormLabel>
                     <Select
-                    id="lessonDay"
-                    placeholder="Select a day"
-                    value={formData.lessonDay}
-                    onChange={handleChange}
-                    fontSize="1rem"
+                      id="lessonDay"
+                      placeholder="Select a day"
+                      value={formData.lessonDay}
+                      onChange={handleChange}
+                      borderRadius="md"
+                      borderColor="gray.300"
+                      _focus={{ borderColor: "cyan.400", boxShadow: "none" }}
+                      fontSize="1rem"
                     >
-                    <option value="wednesday">Wednesday at 6:30 PM (2 hours)</option>
-                    <option value="thursday">Thursday at 6:30 PM (2 hours)</option>
+                      <option value="wednesday">Wednesday at 6:30 PM (2 hours)</option>
+                      <option value="thursday">Thursday at 6:30 PM (2 hours)</option>
                     </Select>
-                </FormControl>
+                  </FormControl>
                 </Stack>
-            </Box>
+              </Box>
 
               {/* Additional Information Section */}
-              <Box bg="white" p={4} 
-               borderWidth={1}
-               borderRadius="lg"
-               boxShadow="lg">
+              <Box
+                bg="white"
+                p={4}
+                borderWidth={1}
+                borderRadius="md"
+                boxShadow="md"
+              >
                 <Heading size="md" mb={4}>Additional Information</Heading>
                 <FormControl id="comments">
-                  <FormLabel fontWeight="bold">Comments</FormLabel>
+                <FormLabel fontSize="lg" fontWeight="medium" color="gray.600">Comments</FormLabel>
                   <Textarea
                     id="comments"
                     value={formData.comments}
                     onChange={handleChange}
                     rows={5}
                     placeholder="Enter any comments or additional information here"
+                    borderRadius="md"
+                    borderColor="gray.300"
+                    _focus={{ borderColor: "cyan.400", boxShadow: "none" }}
                     fontSize="1rem"
                   />
                 </FormControl>
               </Box>
             </Stack>
-          </Box>
+      
         </SimpleGrid>
 
         <Box mt={8}>
           <Button
             type="submit"
-            color="white"
-            bgColor="cyan.500"
+            bgColor="purple.500"
+            color={'white'}
             size="lg"
             width="full"
             isLoading={isSubmitting}
             fontSize="1.2rem"
+            _hover={{ bgColor: "blue.600" }}
           >
             Submit
           </Button>
